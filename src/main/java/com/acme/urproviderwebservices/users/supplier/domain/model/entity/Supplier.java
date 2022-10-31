029cecfda3f75ea758378f570edbf29cd97aaf04
Alejandro Jacobo <105515338+rezself@users.noreply.github.com>
2022-10-31 18:19:29 -0500

    feat(supplier): added supplier model

---
+++ src/main/java/com/acme/urproviderwebservices/users/supplier/domain/model/entity/Supplier.java
@@ -0,0 +1,33 @@
+package com.acme.urproviderwebservices.users.supplier.domain.model.entity;
+
+import com.acme.urproviderwebservices.shared.domain.model.BaseModel;
+import lombok.*;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "suppliers")
+public class Supplier extends BaseModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 60)
+    private String name;
+
+    @NotNull
+    @NotBlank
+    @Column(unique = true)
+    private String email;
+
+    private long phoneNumber;
+}
