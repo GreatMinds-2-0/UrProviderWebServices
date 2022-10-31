b853813b425c3574cd108a693c7430dcf7302ce8
Alejandro Jacobo <105515338+rezself@users.noreply.github.com>
2022-10-31 18:29:28 -0500

    feat(supplier): added supplier service.

---
+++ src/main/java/com/acme/urproviderwebservices/users/supplier/domain/service/SupplierService.java
@@ -0,0 +1,17 @@
+package com.acme.urproviderwebservices.users.supplier.domain.service;
+
+import com.acme.urproviderwebservices.users.supplier.domain.model.entity.Supplier;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface SupplierService {
+    List<Supplier> getAll();
+    Page<Supplier> getAll(Pageable pageable);
+    Supplier getById(Long supplierId);
+    Supplier create(Supplier supplier);
+    Supplier update(Long supplierId, Supplier request);
+    ResponseEntity<?> delete(Long supplierId);
+}
