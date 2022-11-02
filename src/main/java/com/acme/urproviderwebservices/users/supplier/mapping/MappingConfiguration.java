e1f36c66fd5499d4ed04526a2018a421b9bb02a7
Rojoxt <delamarzp@gmail.com>
2022-11-01 21:09:06 -0500

    fix: fixed  mapping configuration

--- src/main/java/com/acme/urproviderwebservices/users/supplier/mapping/MappingConfiguration.java
+++ src/main/java/com/acme/urproviderwebservices/users/supplier/mapping/MappingConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration("supplierMappingConfiguration")
 public class MappingConfiguration {
     @Bean
-    public SupplierMapper supplierMapper{
-        return new SupplierMaper();
+    public SupplierMapper supplierMapper(){
+        return new SupplierMapper();
     }
 }
