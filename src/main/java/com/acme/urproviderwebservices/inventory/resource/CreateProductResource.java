53b82226c181098791249e341a78549774996746
Ronald-Delgado-Del-Castillo <u201810250@upc.edu.pe>
2022-11-04 01:16:03 -0500

    Supplier Products relationship shared classes

--- src/main/java/com/acme/urproviderwebservices/inventory/resource/CreateProductResource.java
+++ src/main/java/com/acme/urproviderwebservices/inventory/resource/CreateProductResource.java
@@ -1,8 +1,15 @@
 package com.acme.urproviderwebservices.inventory.resource;
 
+import lombok.*;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+@Getter
+@Setter
+@With
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreateProductResource {
 
     @NotNull
--- src/main/java/com/acme/urproviderwebservices/users/supplier/api/SupplierProductsController.java
+++ src/main/java/com/acme/urproviderwebservices/users/supplier/api/SupplierProductsController.java
@@ -1,9 +1,13 @@
 package com.acme.urproviderwebservices.users.supplier.api;
 
 import com.acme.urproviderwebservices.inventory.domain.service.ProductService;
+import com.acme.urproviderwebservices.inventory.mapping.ProductMapper;
+import com.acme.urproviderwebservices.inventory.resource.CreateProductResource;
+import com.acme.urproviderwebservices.inventory.resource.ProductResource;
 import com.acme.urproviderwebservices.users.supplier.domain.service.SupplierService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/api/v1/suppliers/{supplierId}/products", produces = "application/json")
@@ -13,8 +17,27 @@ public class SupplierProductsController {
 
     private final ProductService productService;
 
-    public SupplierProductsController(SupplierService supplierService, ProductService productService) {
+    private final ProductMapper mapper;
+
+    public SupplierProductsController(SupplierService supplierService, ProductService productService, ProductMapper mapper) {
         this.supplierService = supplierService;
         this.productService = productService;
+        this.mapper = mapper;
+    }
+
+    @GetMapping
+    public Page<ProductResource> getAllProductsBySupplierId(@PathVariable Long supplierId,
+                                                         Pageable pageable) {
+        return mapper.modelListPage(supplierService.getById(supplierId)
+                .getProducts().stream().toList(), pageable);
+    }
+
+    @PostMapping
+    public ProductResource createProduct(@PathVariable Long supplierId,
+                                         @RequestBody CreateProductResource resource) {
+
+        supplierService.addProductToSupplier(supplierId, resource.getName());
+        return mapper.toResource(productService
+                .getByNameAndSupplierId(resource.getName(), supplierId));
     }
 }
