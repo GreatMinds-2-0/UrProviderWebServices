965b244148b9e3b8e23f85e7c71ba7474263d232
Rillex <epostigo1999@gmail.com>
2022-11-09 18:50:42 -0500

    feat: Resource review added

--- src/main/java/com/acme/urproviderwebservices/sales/resource/CreateReviewResource.java
+++ src/main/java/com/acme/urproviderwebservices/sales/resource/CreateReviewResource.java
@@ -1,4 +1,15 @@
 package com.acme.urproviderwebservices.sales.resource;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 public class CreateReviewResource {
+
+    @NotNull
+    @NotBlank
+    private String title;
+
+
+    private String description;
+
 }
