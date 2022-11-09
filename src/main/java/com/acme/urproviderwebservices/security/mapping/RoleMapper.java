7082ad6b38cc1f3fabd719be65ebaa027dfb949b
alumnos <u202020026@upc.edu.pe>
2022-11-09 18:42:08 -0500

    feat(security): added mapping object

--- src/main/java/com/acme/urproviderwebservices/security/mapping/RoleMapper.java
+++ src/main/java/com/acme/urproviderwebservices/security/mapping/RoleMapper.java
@@ -1,4 +1,22 @@
 package com.acme.urproviderwebservices.security.mapping;
 
-public class RoleMapper {
+import com.acme.urproviderwebservices.security.domain.model.enumeration.Roles;
+import com.acme.urproviderwebservices.shared.mapping.EnhancedModelMapper;
+import org.modelmapper.AbstractConverter;
+import org.modelmapper.Converter;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.Serializable;
+
+public class RoleMapper implements Serializable {
+    @Autowired
+    EnhancedModelMapper mapper;
+
+    Converter<Roles,String> roleToString = new AbstractConverter<Roles, String>() {
+        @Override
+        protected String convert(Roles roles) {
+            return null;
+        }
+    }
+
 }
