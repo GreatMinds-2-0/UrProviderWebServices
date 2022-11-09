22a5653d9afb47d35c5a5cd63345d7615caf5fa2
alumnos <u201810250@upc.edu.pe>
2022-11-09 18:19:47 -0500

    Sales Order domain development

--- src/main/java/com/acme/urproviderwebservices/sales/domain/service/SalesOrderService.java
+++ src/main/java/com/acme/urproviderwebservices/sales/domain/service/SalesOrderService.java
@@ -1,6 +1,13 @@
 package com.acme.urproviderwebservices.sales.domain.service;
 
+import com.acme.urproviderwebservices.sales.domain.model.entity.SalesOrder;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import java.util.List;
 
 public interface SalesOrderService {
+    List<SalesOrder> getAll();
+    Page<SalesOrder> getAll(Pageable pageable);
+    SalesOrder getById(Long salesOrderId);
 }
