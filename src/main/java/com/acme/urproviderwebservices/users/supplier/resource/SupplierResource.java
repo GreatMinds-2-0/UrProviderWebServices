a61b960d5337555c01abc683540ed34b90568aba
alumnos <delamarzp@gmail.com>
2022-11-14 18:01:37 -0500

    feat(supplier): update more properties

--- src/main/java/com/acme/urproviderwebservices/users/supplier/resource/SupplierResource.java
+++ src/main/java/com/acme/urproviderwebservices/users/supplier/resource/SupplierResource.java
@@ -9,7 +9,15 @@ import lombok.*;
 @AllArgsConstructor
 public class SupplierResource {
     private Long id;
-    private String name;
-    private String email;
-    private Long phoneNumber;
+    private String SupplierName ;
+    private String Name ;
+    private String LastName ;
+    private String Email;
+    private String Address ;
+    private long Ruc ;
+    private String Category ;
+    private String Description ;
+    private long Phone ;
+    private String Password ;
+    private long Likes ;
 }
