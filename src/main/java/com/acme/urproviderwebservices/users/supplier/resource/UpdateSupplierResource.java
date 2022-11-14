593c7278e85120c2e05d0f90d52fd7df3132169c
alumnos <delamarzp@gmail.com>
2022-11-14 18:03:40 -0500

    feat(supplier): update supplier resource  more properties

--- src/main/java/com/acme/urproviderwebservices/users/supplier/resource/UpdateSupplierResource.java
+++ src/main/java/com/acme/urproviderwebservices/users/supplier/resource/UpdateSupplierResource.java
@@ -3,6 +3,7 @@ package com.acme.urproviderwebservices.users.supplier.resource;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -15,12 +16,57 @@ public class UpdateSupplierResource {
     @NotNull
     @NotBlank
     @Size(max = 60)
+    private String supplierName;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 60)
     private String name;
 
     @NotNull
     @NotBlank
+    @Size(max = 60)
+    private String lastName;
+
+
+    @NotNull
+    @NotBlank
+    @Column(unique = true)
     private String email;
 
+
+    @NotNull
+    @NotBlank
+    @Size(max = 100)
+    private String address ;
+
+    @NotNull
+    @NotBlank
+    private long ruc ;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 60)
+    private String category ;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 60)
+    private String description ;
+
+    @NotNull
+    @NotBlank
+    private long phone ;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 60)
+    private String password;
+
+    @NotNull
+    @NotBlank
     @Size(max = 200)
-    private Long phoneNumber;
+    private String image;
+
+
 }
