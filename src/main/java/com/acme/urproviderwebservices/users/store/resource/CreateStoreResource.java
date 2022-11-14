8a6c4d7ab8ad217d3c3179925af5b74a26684269
bruno1923 <u201914974@upc.edu.pe>
2022-11-14 18:03:26 -0500

    feat(store): add more properties

--- src/main/java/com/acme/urproviderwebservices/users/store/resource/CreateStoreResource.java
+++ src/main/java/com/acme/urproviderwebservices/users/store/resource/CreateStoreResource.java
@@ -17,11 +17,36 @@ public class CreateStoreResource {
     @NotBlank
     @Size(max = 60)
     private String name;
-
+    @NotNull
+    @NotBlank
+    @Size(max = 60)
+    private String storeName;
     @NotNull
     @NotBlank
     @Column(unique = true)
-    private String email;
-
     private long phoneNumber;
+    @NotNull
+    @NotBlank
+    @Size(max = 60)
+    private String lastName;
+    @NotNull
+    @NotBlank
+    @Size(max = 60)
+    private String address;
+    @NotNull
+    @NotBlank
+    @Size(max = 60)
+    private long phone;
+    @NotNull
+    @NotBlank
+    @Size(max = 60)
+    private String email;
+    @NotNull
+    @NotBlank
+    @Size(max = 60)
+    private String password;
+    @NotNull
+    @NotBlank
+    @Size(max = 60)
+    public String image;
 }
