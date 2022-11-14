2c393a4161266acf58e2144d3adc686b1ad7ff69
alumnos <u202020026@upc.edu.pe>
2022-11-14 18:18:17 -0500

    feat(security): added domain persistence

---
+++ src/main/java/com/acme/urproviderwebservices/security/domain/persistence/UserRepository.java
@@ -0,0 +1,13 @@
+package com.acme.urproviderwebservices.security.domain.persistence;
+
+import com.acme.urproviderwebservices.security.domain.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
+    Optional<User> findByUsername(String username);
+}
