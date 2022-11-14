6bd59e8e0c3a8f4250fb013fa652fd53e7d56bc7
bruno1923 <u201914974@upc.edu.pe>
2022-11-14 17:07:28 -0500

    feat(store): add properties

--- src/main/java/com/acme/urproviderwebservices/users/store/domain/model/entity/Store.java
+++ src/main/java/com/acme/urproviderwebservices/users/store/domain/model/entity/Store.java
@@ -24,11 +24,37 @@ public class Store extends BaseModel {
     @NotBlank
     @Size(max = 60)
     private String name;
-
+    @NotNull
+    @NotBlank
+    @Size(max = 60)
+    private String storeName;
     @NotNull
     @NotBlank
     @Column(unique = true)
     private String email;
+    @NotNull
+    @NotBlank
+    @Size(max = 60)
+    private String LastName;
+    @NotNull
+    @NotBlank
+    @Size(max = 60)
+    private String Address;
+    @NotNull
+    @NotBlank
+    @Size(max = 60)
+    private long Phone;
+    @NotNull
+    @NotBlank
+    @Size(max = 60)
+    private String Email;
+    @NotNull
+    @NotBlank
+    @Size(max = 60)
+    private String Password;
+    @NotNull
+    @NotBlank
+    @Size(max = 60)
+    public String Image;
 
-    private long phoneNumber;
 }
