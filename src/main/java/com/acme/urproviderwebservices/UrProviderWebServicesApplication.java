910f94e29853c24db2d5a00e376354b1658d4af5
alumnos <u202020026@upc.edu.pe>
2022-11-16 18:47:27 -0500

    feat(security): added jwtauthentication

--- src/main/java/com/acme/urproviderwebservices/UrProviderWebServicesApplication.java
+++ src/main/java/com/acme/urproviderwebservices/UrProviderWebServicesApplication.java
@@ -1,9 +1,15 @@
 package com.acme.urproviderwebservices;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@SecurityScheme(name = "urprovider", scheme = "bearer",type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
+@OpenAPIDefinition
 @EnableJpaAuditing
 @SpringBootApplication
 public class UrProviderWebServicesApplication {
