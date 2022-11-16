a9c86a0d2c94f17ff658d8452b80fb005060eaf5
Alejandro Jacobo <delamarzp@gmail.com>
2022-11-16 18:31:45 -0500

    fix(supplier): fixed more supplier resources.

--- src/main/java/com/acme/urproviderwebservices/users/supplier/resource/SupplierResource.java
+++ src/main/java/com/acme/urproviderwebservices/users/supplier/resource/SupplierResource.java
@@ -2,6 +2,11 @@ package com.acme.urproviderwebservices.users.supplier.resource;
 
 import lombok.*;
 
+import javax.persistence.Column;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @With
@@ -10,6 +15,14 @@ import lombok.*;
 public class SupplierResource {
     private Long id;
     private String name;
+    private String lastName;
+    private String image;
     private String email;
-    private Long phoneNumber;
+    private String address;
+    private long ruc;
+    private String category;
+    private String description;
+    private long phone;
+    private String password;
+    private int likes;
 }
--- src/main/java/com/acme/urproviderwebservices/users/supplier/resource/UpdateSupplierResource.java
+++ src/main/java/com/acme/urproviderwebservices/users/supplier/resource/UpdateSupplierResource.java
@@ -3,6 +3,7 @@ package com.acme.urproviderwebservices.users.supplier.resource;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -10,7 +11,6 @@ import javax.validation.constraints.Size;
 @Getter
 @Setter
 public class UpdateSupplierResource {
-    private Long id;
 
     @NotNull
     @NotBlank
@@ -19,8 +19,46 @@ public class UpdateSupplierResource {
 
     @NotNull
     @NotBlank
+    @Size(max = 60)
+    private String lastName;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 200)
+    @Column(unique = true)
+    private String image;
+
+    @NotNull
+    @NotBlank
+    @Column(unique = true)
+    @Size(max = 60)
     private String email;
 
+    @NotNull
+    @NotBlank
+    @Size(max = 60)
+    private String address;
+
+
+    private long ruc;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 60)
+    private String category;
+
+    @NotNull
+    @NotBlank
     @Size(max = 200)
-    private Long phoneNumber;
+    private String description;
+
+
+
+    private long phone;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 20)
+    private String password;
+
 }
