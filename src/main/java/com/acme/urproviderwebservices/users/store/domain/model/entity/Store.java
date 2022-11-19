30a61d92a0d0bb419a4437b256c81bb23814e6b5
Rojoxt <delamarzp@gmail.com>
2022-11-19 15:44:42 -0500

    feat(store): added store resource

--- src/main/java/com/acme/urproviderwebservices/users/store/domain/model/entity/Store.java
+++ src/main/java/com/acme/urproviderwebservices/users/store/domain/model/entity/Store.java
@@ -21,14 +21,36 @@ public class Store extends BaseModel {
     private Long id;
 
     @NotNull
+    @Size(max = 60)
+    private String storeName;
+
+    @NotNull
     @NotBlank
     @Size(max = 60)
     private String name;
+
+
+    @Size(max = 60)
+    private String lastName;
 
     @NotNull
     @NotBlank
     @Column(unique = true)
     private String email;
 
-    private long phoneNumber;
+
+    @Size(max = 60)
+    private String address;
+
+    private long phone;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 20)
+    private String password;
+
+
+    @NotBlank
+    @Size(max = 200)
+    private String image;
 }
--- src/main/java/com/acme/urproviderwebservices/users/store/resource/StoreResource.java
+++ src/main/java/com/acme/urproviderwebservices/users/store/resource/StoreResource.java
@@ -8,7 +8,12 @@ import lombok.*;
 @AllArgsConstructor
 public class StoreResource {
     private Long id;
+    private String storeName;
     private String name;
+    private String lastName;
     private String email;
-    private Long phoneNumber;
+    private String password;
+    private Long phone;
+    private String address;
+    private String image;
 }
